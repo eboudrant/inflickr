ceb8343b766ec70bb803c88a2a58be2f0cea680e
Emmanuel Boudrant <emmanuel.boudrant@gmail.com>
2012-11-05 11:12:21 -0800

    Update www/lib/inflickr.js

--- www/lib/inflickr.js
+++ www/lib/inflickr.js
@@ -3,17 +3,16 @@ var stopScrolling = false;
 var counter = 0;
 var page = 1;
 var gaAccount = 'UA-36103054-1';
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', gaAccount]);
-_gaq.push(['_trackPageview']);
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36103054-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
 function trackImage() {
     _gaq.push(['_trackEvent', 'image', 'view']);
